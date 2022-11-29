fb318309918993fc84870b7adb04d22cd12c0693
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2022-11-29 13:04:40 -0500

    changes 1.01
    just some changes

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import characterData from './data.js'
 import Character from './Character.js'
 
+function attack() {
+    wizard.getDiceHtml()
+    orc.getDiceHtml()
+    wizard.takeDamage(orc.currentDiceScore)
+    orc.takeDamage(wizard.currentDiceScore)
+    render()
+    if(wizard.dead || orc.dead){
+        endGame()
+    }
+}
+function endGame(){
+    console.log('The game is over')
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
 function render() {
-    document.getElementById('hero').innerHTML = wizard.getCharacterHtml();
-    document.getElementById('monster').innerHTML = orc.getCharacterHtml();
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = orc.getCharacterHtml()
 }
 
 const wizard = new Character(characterData.hero)
