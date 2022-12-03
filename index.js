ffa0f898e732123c63ee906264402dbe9e1c3a3e
StudentForEternity <109116999+StudentForEternity@users.noreply.github.com>
2022-12-03 16:30:15 -0500

    improvements

--- index.js
+++ index.js
@@ -16,37 +16,41 @@ function attack() {
     render()
     
     if(wizard.dead){
-       setTimeout(() => {
         endGame()
-       }, 1500); 
     }
+
     else if(monster.dead){
-        setTimeout(() => {
+        
             if(monstersArray.length > 0){
-                monster = getNewMonster()
-                render()
+                setTimeout(() => {
+                    monster = getNewMonster()
+                    render()
+                }, 1500); 
             }
             else{
                 endGame()
             }
-        }, 1500);
+        
     }
 }
 
 function endGame() {
     const endMessage = wizard.health === 0 && monster.health === 0 ?
-        "No victors - all creatures are dead" :
-        wizard.health > 0 ? "The Wizard Wins" :
-            "The Orc is Victorious"
+            "No victors - all creatures are dead" :
+            wizard.health > 0 ? "The Wizard Wins" :
+                "The Orc is Victorious"
 
     const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
-    document.body.innerHTML = `
-        <div class="end-game">
-            <h2>Game Over</h2> 
-            <h3>${endMessage}</h3>
-            <p class="end-emoji">${endEmoji}</p>
-        </div>
-        `
+    
+   setTimeout(() => {
+            document.body.innerHTML = `
+            <div class="end-game">
+                <h2>Game Over</h2> 
+                <h3>${endMessage}</h3>
+                <p class="end-emoji">${endEmoji}</p>
+            </div>
+            `
+   }, 1500); 
 }
 
 document.getElementById("attack-button").addEventListener('click', attack)
